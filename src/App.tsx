326cbe87ae17794da493d110d1608693a17d4716
pakornds <souldragonprite@gmail.com>
2023-07-23 14:36:51 +0700

    nmice

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
+import About from './about'
 
-const data: string[] = ['a', 'b', 'c', 'd', 'e', 'f'];
-
+const data: string[] = ["a", "b", "c", "d"]
+//น้องคนข้างหลังน่ารักที่สุดฮะ 
 function App() {
-  const [test, setTest] = useState<string>("Test")
-
+  const [test, setTest] = useState<string>("Test");
   return (
-      <div className = "main">
-        <h1>hello react</h1>
-        <About data={data} func={setTest} />
-        <h1>{test}</h1>
-        input onChange=((e) => setTest(test + e.target.value))
-      </div>
+    <div className="main">
+      <h1>hello react</h1>
+      <h2>{test}</h2>
+      <About data={data} func={setTest} />
+      {data.map((item, idx) => {
+        if (item === "a") return <h1 key={idx}>AAA</h1>;
+        else return <h4 key={idx}>{item}</h4>;
+      })}
+    </div>
   )
 }
 
-export default App
+export default App;
