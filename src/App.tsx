6cd043c683185aeccc25009e29a76ec0f612685a
pakornds <souldragonprite@gmail.com>
2023-07-23 14:41:28 +0700

    fix filename

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import About from './about'
+import About from './About'
 
 const data: string[] = ["a", "b", "c", "d"]
 //น้องคนข้างหลังน่ารักที่สุดฮะ 
